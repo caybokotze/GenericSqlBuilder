a00a8372d04970e5cbe6b6ee058ca6be74d3f664
Caybo Kotzé <kotzecabo@gmail.com>
2021-09-09 22:53:38 +0200

    :white_check_mark: fleshing out issues, started writing tests

---
+++ GenericSqlBuilder.Tests/SqlBuilderTests.cs
@@ -0,0 +1,126 @@
+﻿using NExpect;
+using NUnit.Framework;
+using static NExpect.Expectations;
+
+namespace GenericSqlBuilder.Tests
+{
+    [TestFixture]
+    public class SqlBuilderTests
+    {
+        public class Person
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public string Email { get; set; }
+        }
+        
+        [TestFixture]
+        public class SelectStatementTests
+        {
+            [Test]
+            public void ShouldReturnSelectLastInsertedForMySql()
+            {
+                // arrange
+                var sql = new SqlBuilder()
+                    .Select()
+                    .LastInserted(For.MySql);
+                // act
+                // assert
+                Expect(sql).To.Equal("SELECT LAST_INSERT_ID();");
+            }
+
+            [TestFixture]
+            public class GenericBuilder
+            {
+                [Test]
+                public void TestBasicSelect()
+                {
+                    // arrange
+                    var sql = new SqlBuilder()
+                        .Select<Person>()
+                        .Build(For.MySql);
+                    // act
+                    // assert
+                    Expect(sql).To.Equal("SELECT id, name, surname, email;");
+                }
+
+                [Test]
+                public void ShouldReturnFullSelect()
+                {
+                    // arrange
+                    var sql = new SqlBuilder()
+                        .Select<Person>()
+                        .From("people")
+                        .Where("id")
+                        .Equals("1")
+                        .Build(For.MySql);
+                    // act
+                    // assert
+                    Expect(sql).To.Equal("SELECT id, name, surname, email FROM people WHERE id = 1;");
+                }
+
+                [Test]
+                public void ShouldReturnSelectWithAndClause()
+                {
+                    // arrange
+                    var sql = new SqlBuilder()
+                        .Select<Person>()
+                        .From("people")
+                        .Where("id")
+                        .Equals("1")
+                        .And("name")
+                        .Like("%John%")
+                        .Build(For.MySql);
+                    // act
+                    // assert
+                    Expect(sql).To.Equal("SELECT id, name, surname, email FROM people WHERE id = 1;");
+                }
+
+                [TestFixture]
+                public class IgnorePropertiesTests
+                {
+                    [Test]
+                    public void ShouldBuildSelectWithRemovedProperties()
+                    {
+                        // arrange
+                        // act
+                        var sql = new SqlBuilder()
+                            .Select<Person>(o => o.IgnoreProperty(nameof(Person.Name)));
+                        // assert
+                        Expect(sql).To.Equal("")
+                    }
+                }
+                
+                [Test]
+                public void ShouldBuildSelectWithJoin()
+                {
+                    // arrange
+                    var sql = new SqlBuilder()
+                        .Select<Person>()
+                        .From("people")
+                        .Where("id")
+                        .Equals("1")
+                        .And("name")
+                        .Like("%John%")
+                        .Build(For.MySql);
+                    // act
+                    // assert
+                    Expect(sql).To.Equal("");
+                }
+
+                [TestFixture]
+                public class CasingTests
+                {
+                    
+                }
+            }
+        }
+
+        [TestFixture]
+        public class StatementTests
+        {
+            
+        }
+    }
+}
--- GenericSqlBuilder/SqlBuilder.cs
+++ GenericSqlBuilder/SqlBuilder.cs
@@ -2,19 +2,17 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Threading;
+using PeanutButter.Utils;
 
 namespace GenericSqlBuilder
 {
-
     public class Test
     {
-
         public int Name { get; set; }
         void Foo()
         {
-            new SqlBuilder()
+            var statement = new SqlBuilder()
                 .Select<Test>(o =>
                 {
                     o.UsePropertyCase(Casing.PascalCase);
@@ -24,8 +22,7 @@ namespace GenericSqlBuilder
                 .Where("id")
                 .Equals("@Id")
                 .And("name")
-                .Like("@Name")
-                .Build(For.MySql);
+                .Like("@Name");
         }
     }
 
@@ -36,10 +33,17 @@ namespace GenericSqlBuilder
     public class SqlBuilder
     {
         private string _sql;
+        private SqlStatementOptions _sqlStatementOptions;
 
         public SqlBuilder()
         {
             _sql = new string("");
+            _sqlStatementOptions = new SqlStatementOptions();
+        }
+        
+        public SelectStatement Select()
+        {
+            return new SelectStatement("");
         }
 
         public SelectStatement Select(string properties)
@@ -65,6 +69,7 @@ namespace GenericSqlBuilder
         private string CreateSelectStatement<T>(List<string> remove = null)
         {
             var dataTable = GenericPropertyBuilder<T>.GetGenericProperties();
+            
             if (remove != null && remove.Count > 0)
             {
                 foreach (var item in remove)
@@ -73,13 +78,21 @@ namespace GenericSqlBuilder
                 }
             }
 
-            var variables = dataTable.Columns
-                .Cast<object>()
-                .Aggregate("", (current, _) => string.Join(", ", current));
+            var variableArray = new string[dataTable.Columns.Count];
+            for (var i = 0; i < dataTable.Columns.Count; i ++)
+            {
+                variableArray[i] = _sqlStatementOptions.Casing switch
+                {
+                    Casing.UpperCase => dataTable.Columns[i].ColumnName.ToUpperInvariant(),
+                    Casing.LowerCase => dataTable.Columns[i].ColumnName.ToLowerInvariant(),
+                    Casing.PascalCase => dataTable.Columns[i].ColumnName.ToPascalCase(),
+                    Casing.KebabCase => dataTable.Columns[i].ColumnName.ToKebabCase(),
+                    Casing.SnakeCase => dataTable.Columns[i].ColumnName.ToSnakeCase(),
+                    _ => dataTable.Columns[i].ColumnName
+                };
+            }
 
-            variables = variables.Remove(variables.Length, -2);
-
-            return $"({variables})";
+            return string.Join(", ", variableArray) + " ";
         }
     }
 
@@ -88,14 +101,15 @@ namespace GenericSqlBuilder
         public SqlStatementOptions()
         {
             _propertiesToIgnore = new List<string>();
+            Casing = Casing.LowerCase;
         }
         
-        private Casing _casing;
+        public Casing Casing;
         private readonly List<string> _propertiesToIgnore;
 
         public void UsePropertyCase(Casing casing)
         {
-            _casing = casing;
+            Casing = casing;
         }
 
         public List<string> FetchIgnoredProperties()
@@ -137,13 +151,30 @@ namespace GenericSqlBuilder
     }
     
 
-    public class Statement
+    public class Statement : IStatement
     {
         private List<string> _statements;
 
         protected Statement()
         {
             _statements = new List<string>();
+        }
+        
+        public Statement(List<string> statements)
+        {
+            _statements = statements;
+        }
+
+        protected string GenerateSqlStatement()
+        {
+            var result = "";
+            _statements.ForEach(f =>
+            {
+                result += string.Join(" ", f);
+            });
+            result = result.Trim();
+            result += ";";
+            return result;
         }
 
         protected void AddStatement(string statement)
@@ -156,7 +187,7 @@ namespace GenericSqlBuilder
 
         public string Build(For @for)
         {
-            return "";
+            return GenerateSqlStatement();
         }
         
         public Statement From(string table)
@@ -200,6 +231,58 @@ namespace GenericSqlBuilder
             AddStatement("IS ");
             return this;
         }
+
+        public JoinStatement LeftJoin(string table)
+        {
+            AddStatement($"LEFT JOIN {table}");
+            return new JoinStatement(_statements);
+        }
+
+        public JoinStatement RightJoin(string table)
+        {
+            AddStatement($"RIGHT JOIN {table}");
+            return new JoinStatement(_statements);
+        }
+
+        public JoinStatement InnerJoin(string table)
+        {
+            AddStatement($"INNER JOIN {table}");
+            return new JoinStatement(_statements);
+        }
+    }
+
+    public interface IStatement
+    {
+        private void AddStatement(string statement) { }
+    }
+
+    public class JoinStatement : IStatement
+    {
+        private List<string> _statements;
+        public JoinStatement(List<string> statements)
+        {
+            _statements = statements;
+        }
+
+        public JoinStatement On(string property)
+        {
+            AddStatement($"ON {property} ");
+            return this;
+        }
+
+        public Statement Equals(string property)
+        {
+            AddStatement($"= {property} ");
+            return new Statement(_statements);
+        }
+
+        private void AddStatement(string statement)
+        {
+            var semaphore = new SemaphoreSlim(1, 1);
+            semaphore.Wait();
+            _statements.Add(statement);
+            semaphore.Release();
+        }
     }
 
     public class SelectStatement : Statement
@@ -207,6 +290,13 @@ namespace GenericSqlBuilder
         public SelectStatement(string properties)
         {
             AddStatement("SELECT ");
+            AddStatement(properties);
+        }
+
+        public string LastInserted(For @for)
+        {
+            AddStatement("LAST_INSERT_ID() ");
+            return GenerateSqlStatement();
         }
     }
 
